e1d3da51c15e6e20f100788fd574805fce231c9c
student <student@localhost.localdomain>
2016-05-16 15:25:01 +0800

    bug fix

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ DIR_SUB = ./subroutines
 SRC = $(wildcard $(DIR_SRC)/*.f90)
 
 
-all : hw23 hw24 hw31 hw32 hw34 hw41
+all : hw23 hw24 hw31 hw32 hw34 hw41 hw42
 
 hw23 : hw_2_3.o
 	$(FC) $(FLAGS) -o $@ $^
@@ -45,11 +45,17 @@ hw_3_4.o : $(DIR_SRC)/hw_3_4.f90
 hw41 : typedef.o hw_4_1.o 
 	$(FC) $(FLAGS) -o $@ $^
 	mv hw_4_1.o ./obj/hw_4_1.o
-	mv typedef.mod ./obj/typedef.mod
 hw_4_1.o : $(DIR_SRC)/hw_4_1.f90
+	$(FC) $(FLAGS) -c $^
+
+hw42 :  typedef.o hw_4_2.o
+	$(FC) $(FLAGS) -o $@ $^
+	mv hw_4_2.o ./obj/hw_4_2.o
+hw_4_2.o : $(DIR_SRC)/hw_4_2.f90
 	$(FC) $(FLAGS) -c $^
 typedef.o : $(DIR_SUB)/typedef.f90
 	$(FC) $(FLAGS) -c $^
+
 
 clean:
 	rm -f *.o *.mod *.MOD
--- homeworks/hw_9_3.f90
+++ homeworks/hw_9_3.f90
@@ -1,13 +1,20 @@
-	    program main
-	    use interp
-	    real:: ix,iy
-	    real:: xd(3),yd(3)
+	     program main
+	       use interp
+	     real,allocatable :: ix(:),iy(:)
+	     real:: xd(3),yd(3)
 
-	    data xd /1.,2.,3./
-	    data yd /2.,3.,1./
+ 	     data xd /1.,2.,3./
+	     data yd /2.,3.,1./
+ 
+         write(*,*) '***************'
+         write(*,*) 'input num data'
+         write(*,*) '***************'
+         read(*,*) numdata
+         write(*,*) 'input data'
+	     allocate(ix(numdata))
+         allocate(iy(numdata))
+         read(*,*) ix
+	     call lag_interp(xd,yd,ix,iy)
+	     write(*,*) iy
 
-	    read(*,*) ix
-	    call lag_interp(xd,yd,ix,iy)
-	    write(*,*) iy
-
-	    end
+	     end
